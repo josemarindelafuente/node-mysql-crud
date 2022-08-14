e0c9e5aaed24bf5ef5ea72cfc393583603277ad2
jmarin <jose.marindelafuente@gmail.com>
2022-08-13 23:46:40 -0300

    adminlte y template en componentes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path =require("path");
 
 app.set("view engine", "ejs");
 
@@ -8,6 +9,7 @@ app.use(express.urlencoded({extended:false}));
 // import from router
 app.use("/", require("./routes/router")),
 
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(5000, ()=>{
     console.log("server corriendo en el puerto 5000");
